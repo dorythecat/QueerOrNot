06a288da0a864cbe41b4915306a9c924261e64ab
Dory <pemegomachado@gmail.com>
2025-06-23 22:23:36 +0200

    Added game.html

--- res/script.js
+++ res/script.js
@@ -6,3 +6,16 @@ function answer(id) {
     answerElement.style.display = "none";
   }
 }
+
+function startGame(queer) {
+  const activeElement = document.getElementsByClassName("game active")[0];
+  const inactiveElement = document.getElementsByClassName("game inactive")[0];
+  if (activeElement) {
+    activeElement.classList.remove("active");
+    activeElement.classList.add("inactive");
+  }
+  if (inactiveElement) {
+    inactiveElement.classList.remove("inactive");
+    inactiveElement.classList.add("active");
+  }
+}
